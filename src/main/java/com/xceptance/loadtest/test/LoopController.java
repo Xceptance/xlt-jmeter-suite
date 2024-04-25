03197a3058097a21588b111ff5b84a31cbfaa21e
DK <d.kirst@xceptance.net>
2024-04-25 10:34:32 +0200

    Added tests for TransactionController and LoopController

---
+++ src/main/java/com/xceptance/loadtest/test/LoopController.java
@@ -0,0 +1,9 @@
+package com.xceptance.loadtest.test;
+
+public class LoopController extends AbstractComponentTest
+{
+    public LoopController()
+    {
+        jmxSource = "/tests/LoopController.jmx";
+    }
+}
---
+++ src/main/java/com/xceptance/loadtest/test/TransactionController.java
@@ -0,0 +1,9 @@
+package com.xceptance.loadtest.test;
+
+public class TransactionController extends AbstractComponentTest
+{
+    public TransactionController()
+    {
+        jmxSource = "/tests/TransactionController.jmx";
+    }
+}
