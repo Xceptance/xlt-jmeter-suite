566142901e4810e37bbc31f8f063c8b09eb392d1
TV <t.volkmann@xceptance.net>
2024-04-25 11:58:27 +0200

    assertion test

---
+++ src/main/java/com/xceptance/loadtest/test/AssertionsTest.java
@@ -0,0 +1,9 @@
+package com.xceptance.loadtest.test;
+
+public class AssertionsTest extends AbstractComponentTest
+{
+    public AssertionsTest()
+    {
+        jmxSource = "/tests/Assertion_Check.jmx";
+    }
+}
