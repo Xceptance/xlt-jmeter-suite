994f28ce2e8f07d7345bd5260d2d72c7722f2a53
DK <d.kirst@xceptance.net>
2024-04-29 09:35:11 +0200

    Added test case for nestings of controllers [wip]

---
+++ src/main/java/com/xceptance/loadtest/test/NestingsTest.java
@@ -0,0 +1,9 @@
+package com.xceptance.loadtest.test;
+
+public class NestingsTest extends AbstractComponentTest
+{
+    public NestingsTest()
+    {
+        jmxSource = "/tests/Nestings.jmx";
+    }
+}
