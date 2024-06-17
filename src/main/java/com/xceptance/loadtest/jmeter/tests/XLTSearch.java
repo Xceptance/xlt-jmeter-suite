40eb1ed490341d8650098d7be4f1c7c0799afc3d
Fabian Heydrich <fheydrich@gmx.de>
2024-06-17 14:51:38 +0200

    First draft for search test case

---
+++ src/main/java/com/xceptance/loadtest/jmeter/tests/XLTSearch.java
@@ -0,0 +1,12 @@
+package com.xceptance.loadtest.jmeter.tests;
+
+import com.xceptance.loadtest.test.AbstractComponentTest;
+
+public class XLTSearch extends AbstractComponentTest
+{
+  public XLTSearch()
+  {
+    useRequestMode = false;
+    jmxSource = "/search.jmx";
+  }
+}
