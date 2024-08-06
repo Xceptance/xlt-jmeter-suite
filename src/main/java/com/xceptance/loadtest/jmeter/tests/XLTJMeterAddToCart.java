213edc8b8121c923c4ba1e6a698b51f0de6e39da
Fabian Heydrich <fheydrich@gmx.de>
2024-08-06 11:19:50 +0200

    Add addToCart testcase

---
+++ src/main/java/com/xceptance/loadtest/jmeter/tests/XLTJMeterAddToCart.java
@@ -0,0 +1,11 @@
+package com.xceptance.loadtest.jmeter.tests;
+
+import com.xceptance.loadtest.test.AbstractComponentTest;
+
+public class XLTJMeterAddToCart extends AbstractComponentTest
+{
+    public XLTJMeterAddToCart()
+    {
+        jmxSource ="/tests/addToCart.jmx";
+    }
+}
