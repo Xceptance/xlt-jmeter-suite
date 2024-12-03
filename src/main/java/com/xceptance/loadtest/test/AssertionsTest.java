668f3ff57cd7623496e14743e95dbd1271747e00
TV <t.volkmann@xceptance.net>
2024-12-03 13:16:45 +0100

    adjusted tests and jmx files

--- src/main/java/com/xceptance/loadtest/test/AssertionsTest.java
+++
@@ -1,9 +0,0 @@
-package com.xceptance.loadtest.test;
-
-public class AssertionsTest extends AbstractComponentTest
-{
-    public AssertionsTest()
-    {
-        jmxSource = "/tests/Assertion_Check.jmx";
-    }
-}
--- src/main/java/com/xceptance/loadtest/test/PostProcessorTest.java
+++
@@ -1,9 +0,0 @@
-package com.xceptance.loadtest.test;
-
-public class PostProcessorTest extends AbstractComponentTest
-{
-    public PostProcessorTest()
-    {
-        jmxSource = "/tests/GuestOrder_PostProcessor.jmx";
-    }
-}
